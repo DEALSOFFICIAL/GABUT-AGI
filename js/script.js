30b0292a42b3e40186bfd38fa9b2afe0fd706b08
DEALSOFFICIAL <deals8544@gmail.com>
2026-02-18 18:12:05 +0700

    script.js

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ function getNama(){
 
 function timeoutData() {
     setTimeout(function(){
-        window.location.href = "https://api.whatsapp.com";
+        window.location.href = "https://whatsapp.com/channel/0029VbCPX8aChq6Mz9uyPK1r";
     },82000)
 }
+
